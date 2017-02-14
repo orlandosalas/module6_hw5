e091f2e34622b69831ae42301e55bdb188493334
Orlando Salas <orlandosalas@mail.weber.edu>
2017-02-14 16:39:14 +0000

    Initial commit for task2

---
+++ protons_hw5_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  protons_hw5_task2.c
+ *       	Usage:  ./protons_hw5_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:43 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Orlando Salas (), orlandosalas@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
