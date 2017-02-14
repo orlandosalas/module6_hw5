81f3a9dca4e0c2c951623e4237a4d1b9ce82e085
Tyler Madsen <madsentyler@mail.weber.edu>
2017-02-14 16:51:21 +0000

    initial

---
+++ House_Proton_hw5_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  House_Proton_hw5_task1.c
+ *       	Usage:  ./House_Proton_hw5_task1.c
+ *    Description:  yay
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:45:52 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tyler Madsen (), tylermadsen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
