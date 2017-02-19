f849d7b666b588904aa5bff8f8e27395f98718f3
Orlando Salas <orlandosalas@mail.weber.edu>
2017-02-19 00:40:45 +0000

    finshed task2, might need alignment

--- protons_hw5_task2.c
+++ protons_hw5_task2.c
@@ -14,15 +14,45 @@
 #include <stdio.h>		/* For Standard I/O */
 
 /* Function Prototypes */
+float MitoKi(int m1); // expects an integer and returns a float
+
 
 /* Main Program */
 int main(void)
 {
+	int inmi, enmi, incra; // miles: start and finish
+	float k2; // ouput (kilometers) as float
 
+	printf("We are going to convert miles to kilometers.\nPlease enter a mile value: ");
+	scanf("%d", &inmi);
+	printf("Enter your ending mile: ");
+	scanf("%d", &enmi);
+	printf("Enter a desired incrament for your conversions: ");
+	scanf("%d", &incra);
+	
+
+	printf("Miles = Kilometers\n");
+	printf("-----   ----------\n");
+
+	// increment
+	
+	for(int i = inmi; i <= enmi  ; i+=incra)// last part indicates that
+											//loop icraments by desired incrament 
+	{
+		k2 = MitoKi(inmi); // calls function that expects a data return w/ float
+		printf("%3d   %8.2f\n", inmi, k2);
+		inmi = inmi + incra;
+	}
 	return 0;
 }
 
 
 /* Function Defenitions */
+float MitoKi(int m1)
+{
+	float k2;
+	k2 = m1 * 1.61; // takes what is stored. note: notice how it is m1 and not inmi
 
+	return k2; // returns to main
+}
 
